79ed91fac089123e2616864751e2106ae1a1549b
haruntahid <tahidtaha997@gmail.com>
2024-08-17 14:41:20 +0600

    cors issue solved

--- index.js
+++ index.js
@@ -7,15 +7,13 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleware
 app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "https://mobile-mart-d9ffe.web.app",
-    ],
-  })
-);
+
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
+app.use(cors(corsOptions));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jc69b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
